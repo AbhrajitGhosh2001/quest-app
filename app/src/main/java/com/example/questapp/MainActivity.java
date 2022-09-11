a4f52ad74e4105462c711ded490e9f99263f6e69
AbhrajitGhosh2001 <abhrajit_ghosh@outlook.com>
2022-09-11 00:01:02 -0400

    first

---
+++ app/src/main/java/com/example/questapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.questapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username=(TextView) findViewById(R.id.username);
+        TextView password=(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(username.getText().toString().equals("joe")&& password.getText().toString().equals("joe")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }
+                else if(username.getText().toString().equals("don")&& password.getText().toString().equals("don")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
